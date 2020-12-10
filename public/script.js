4a5f65c0813bbdc41acaf4951e3fe0dbbb1c3267
alegonz320 <70660097+alegonz320@users.noreply.github.com>
2020-12-09 23:15:36 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+//it's either this one
+
 const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 const stuff = [];
 
@@ -12,3 +14,22 @@ function findMatches(wordtoMatch, stuff){
 });
 
 }
+
+//or this one. with this one u change the link in server.js fetch to our api link
+//and then obviously u wud change the 'submit' event listener to the 'reset' event listener
+document.body.addEventListener('submit', async (e) => {
+    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+    const form = $(e.target).serializeArray();
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((fromServer) => fromServer.json())
+      .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
