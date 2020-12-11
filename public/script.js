65511efb9217bc7e51d3ee15dbad1b33d54e9d75
Michael Rakhamimov <mer0409@gmail.com>
2020-12-10 20:29:05 -0500

    Filter

--- public/script.js
+++ public/script.js
@@ -1,47 +1,34 @@
-import { Loader } from "@googlemaps/js-api-loader"
-const loader = new Loader({
-  apiKey: "AIzaSyDrBA-BFDn-pRnv1tdx-iQMNCFSAsX3kFg",
-  version: "weekly",
-  ...additionalOptions,
-});
-loader.load().then(() => {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-});
-
 const endpoint = "https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json";
-const stuff = [];
+const crimes = [];
 
-fetch (endpoint)
-.then(blob => blob.json())
-.then(data => stuff.push(...data))
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => crimes.push(...data))
 
-function findMatches(wordtoMatch, stuff){ 
-  return stuff.filter(resta =>{
-    const regex = new RegExp(wordtoMatch, 'gi');
-    return resta.category.match(regex) || resta.name.match(regex)
-});
-
+function findMatches(wordToMatch, restaurants) {
+    return crimes.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.street_address.match(regex) || place.date.match(regex)
+    });
 }
 
-let map, heatmap;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: { lat: 37.775, lng: -122.434 },
-    mapTypeId: "satellite",
-  });
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: getPoints(),
-    map: map,
-  });
+function displayMatches(){
+    const matchArray = findMatches(this.value, restaurants);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class="street_address">${place.street_address}<br></span>
+                <span class="clearance_code_inc_type">${place.clearance_code_inc_type}<br></span>
+                <span class="date">${place.date}<br></span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
 
-function getPoints() {
-  return [
-    new google.maps.LatLng(38.9821313173, -76.9378171116)
-  ]
-}
+const searchInput = document.querySelector('.search');
+
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
