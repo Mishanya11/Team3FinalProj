07cd17ca858acaaa054b559452d0c3c14615ec37
zcollin <40807332+zcollin@users.noreply.github.com>
2020-12-11 19:28:07 -0500

    update

--- public/script.js
+++ public/script.js
@@ -55,7 +55,6 @@ function runThisAfterEvent(input, jsonFromServer) {
 document.body.addEventListener('input', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   input = document.querySelector('input').value;
-  console.log(input);
   if(input == '') {
     ul = document.querySelector('ul')
     ul.innerHTML = '';
@@ -64,48 +63,7 @@ document.body.addEventListener('input', async (e) => {
   }
 });
 
-
-
-
-
-
-
-
 /*
-
-
-function findMatches(wordtoMatch, stuff){ 
-  return stuff.filter(resta =>{
-    const regex = new RegExp(wordtoMatch, 'gi');
-    return resta.clearance_code_inc_type.match(regex) || resta.date.match(regex)
-});
-
-}
-function displayMatches() {
-    const matchArray = findMatches(this.value, stuff);
-    const html = matchArray.map(resta => {
-        return `
-        <li>
-            <span class ="address2">${resta.street_number}</span>
-            <span class ="address">${resta.street_address}</span>
-            <span class = "crime">${resta.clearance_code_inc_type}</span>
-            <span class = "date">${resta.date}</span>
-            <span class = "incidentID">${resta.incident_case_id}</span>
-        </li>
-        `;
-    }).join('');
-    results.innerHTML = html;
-}
-const searchInput = document.querySelector('input');
-const results = document.querySelector(".results");
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
-
-function find_id(stuff){
-  return stuff.incident_case_id;
-}
-
 var heatMapPoints = [];
 json.features.forEach(function find_id(stuff) {
   count_crime = stuff.count(stuff.clearance_code_inc_type);
